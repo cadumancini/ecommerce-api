1ed0fabad4f5e7398dd931ff2eb170f3a56f59bc
Carlos Mancini <carlos@shipium.com>
2026-04-22 08:23:22 -0300

    Switch string to single quote.

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): { message: string } {
-    return { message: "E-Commerce API is running" };
+    return { message: 'E-Commerce API is running' };
   }
 }
